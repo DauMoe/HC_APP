26f3c90adab5aab49be845afd48be5768b73d5c2
Hue Nguyen <nguyenthihue1301tb@gmail.com>
2021-09-24 13:46:00 +0700

    update loading

--- app/src/main/java/com/example/hc_app/SignupActivity.java
+++ app/src/main/java/com/example/hc_app/SignupActivity.java
@@ -1,10 +1,15 @@
 package com.example.hc_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    AppCompatButton btnRegister;
+    EditText username, password, repeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
