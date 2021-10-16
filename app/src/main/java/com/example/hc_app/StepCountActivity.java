5f3e4becf1aad4a9463093ef9723326ac2f018b5
phamh <phamhoaihk1112@gmail.com>
2021-10-17 01:47:45 +0700

    update

--- app/src/main/java/com/example/hc_app/StepCountActivity.java
+++ app/src/main/java/com/example/hc_app/StepCountActivity.java
@@ -10,13 +10,16 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.ArrayMap;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.AnyThread;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -26,6 +29,7 @@ import com.example.hc_app.Services.APIConfig;
 import com.example.hc_app.Services.RetrofitConfig;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.material.internal.ViewUtils;
 
 import org.json.JSONObject;
 
@@ -45,7 +49,7 @@ import static com.example.hc_app.Models.Config.USER_ID;
 
 public class StepCountActivity extends FragmentActivity {
     AppCompatButton step_stop;
-    TextView step_counter, step_distance;
+    TextView step_counter, step_distance, step_time;
     LinearLayout step_area;
     private boolean mIsBind;
     private SharedPreferences pref;
@@ -73,18 +77,20 @@ public class StepCountActivity extends FragmentActivity {
         step_counter    = findViewById(R.id.step_counter);
         step_area       = findViewById(R.id.step_area);
         step_distance   = findViewById(R.id.step_distance);
+        step_time       = findViewById(R.id.step_time);
         pref            = getApplicationContext().getSharedPreferences(LOGIN_DATA, MODE_PRIVATE);
         starttimestamp  = Calendar.getInstance().getTimeInMillis();
+        endtimestamp    = Calendar.getInstance().getTimeInMillis();
         step_range      = pref.getFloat(STEPRANGE, 0f);
         p               = new ProgressDialog(this);
         curStep         = 0;
 
         step_counter.setText("0");
         step_distance.setText("0");
+        step_time.setText("0");
         showStepCount(0, 0);
         setupService();
         step_stop.setOnClickListener(v -> UpdateSteps());
-
     }
 
     private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
@@ -124,12 +130,46 @@ public class StepCountActivity extends FragmentActivity {
                 public void onServiceDisconnected(ComponentName componentName) {}
             };
 
+
     public void showStepCount(int totalStepNum, int currentCounts) {
         if (currentCounts < totalStepNum) {
             currentCounts = totalStepNum;
         }
         step_counter.setText(currentCounts + "");
         step_distance.setText(String.format("%2f", (float) (currentCounts * step_range)));
+//        long t = endtimestamp - starttimestamp;
+//        String s1 = String.valueOf(t);
+//        s1 = s1+" ms";
+//        step_time.setText(s1);
+        CountUpTimer timer = new CountUpTimer(300000) {
+            public void onTick(int second) {
+                step_time.setText(String.valueOf(second)+"s");
+            }
+        };
+
+        timer.start();
+    }
+    public abstract class CountUpTimer extends CountDownTimer {
+        private static final long INTERVAL_MS = 1000;
+        private final long duration;
+
+        protected CountUpTimer(long durationMs) {
+            super(durationMs, INTERVAL_MS);
+            this.duration = durationMs;
+        }
+
+        public abstract void onTick(int second);
+
+        @Override
+        public void onTick(long msUntilFinished) {
+            int second = (int) ((duration - msUntilFinished) / 1000);
+            onTick(second);
+        }
+
+        @Override
+        public void onFinish() {
+            onTick(duration / 100);
+        }
     }
 
     private void setupService() {
